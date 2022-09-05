d65b51c2e6072f74a7b4c2336ad9f921e370c3a0
niuqiang <niuqiang@sensetime.com>
2022-09-05 16:11:05 +0800

    简化部署逻辑

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
-<?php
-header("location:http://www.sandcomp.com/blog/sandphoto/");
-?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script type="text/javascript" src="sandphoto.js"></script>
+</head>
+<body>
+
+
+<form id="sandphotoform" action="/sandphoto.php" method="POST" enctype="multipart/form-data">
+    <strong>第一步</strong>, 选择你要冲洗的证件照片的尺寸：
+    <?php
+    include('sandphoto.inc');
+    $parser = new PhotoTypeParser();
+    $parser->parse('phototype.txt');
+    $parser->render_select('target_type', 0, 8);
+    echo '<br>';
+    print('<strong>第二步</strong>, 选择用多大的照片冲洗（一般选择6寸的就好，这个价格最合适）:');
+    $parser->render_select('container_type', 8);
+    ?>
+
+    <br>
+    <strong>第三步</strong>, 选择分割线的颜色：
+
+    <input id="bgcolorid" type="radio" name="bgcolorid" value="blue" checked="checked"/> 蓝色 <input id="bgcolorid"
+                                                                                                     type="radio"
+                                                                                                     name="bgcolorid"
+                                                                                                     value="white"/>白色
+    <input id="bgcolorid" type="radio" name="bgcolorid" value="gray"/> 灰色
+
+
+    将来的照片就是这个样子的：<br>
+
+    <img id="previewImg" alt=""/>
+    <br>
+    <strong>第四步</strong>, 选择你的证件照片：
+
+    <input id="filename" type="file" name="filename"/>
+
+    <br>
+    <strong>最后一步</strong>, 点击下载，照片就可以去冲印了：
+
+    <input type="submit" value="下载"/>
+
+    如果你的照片比较大， 上传会花一些时间， 别着急， 请耐心等待。
+
+</form>
+</body>
+</html>
